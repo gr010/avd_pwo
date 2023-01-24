344fe0e51e99925a47af6a796ef7e32f38dae48d
gr010 <121790676+gr010@users.noreply.github.com>
2023-01-24 07:38:55 +0100

    Update CustomLanguage.ps1

--- customize/CustomLanguage.ps1
+++ customize/CustomLanguage.ps1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+Script to install and configure Language Packs via Install-Language
+.DESCRIPTION
+This script will use the new cmdlets for installing and configuring language packs See https://docs.microsoft.com/nl-nl/powershell/module/languagepackmanagement/?view=windowsserver2022-ps for more information. For this script to work you need at least Wind
+.PARAMETER languagePack
+This is the language code for the language(s) you want to install
+.PARAMETER defaultLanguage
+This is the language code for the default language you want to configure
+.DOCS
+Available languages: https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/available-language-packs-for-windows?view=windows-11
+New PowerShell cmdlets: https://docs.microsoft.com/nl-nl/powershell/module/languagepackmanagement/?view=windowsserver2022-ps
+.LINK
+https://github.com/StefanDingemanse/NMW/edit/main/scripted-actions/windows-script/Install%20languages.ps1
+#>
+
 # Customize the following variables
 $languagePacks="en-US","nl-NL"
 $defaultLanguage="nl-NL"
